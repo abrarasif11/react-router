f0264620252f7529ba3145534783b22e2b65ce9b
MD Fahim Abrar Asif <faasif929@gmail.com>
2025-09-27 21:21:07 +0600

    Update Users

--- src/component/User/User.jsx
+++ src/component/User/User.jsx
@@ -6,7 +6,7 @@ const User = () => {
     const users = useLoaderData();
   return (
     <div>
-      <h2>The Users...</h2>
+      <h2>Here is the users </h2>
       <div>
         {
             users.map(user => <Users
